6aecc4b0f23e86e90b6f591c16b1c56056db5d5e
Paul Rezzonico <paul.rezzonico@gmail.com>
2023-10-11 09:02:02 +0200

    refact(MainActivity) refact activity

--- app/src/main/java/com/unilim/iut/truckers/MainActivity.kt
+++ app/src/main/java/com/unilim/iut/truckers/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.unilim.iut.truckers
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.annotation.RequiresApi
+import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import android.app.AlarmManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.IntentFilter
-import android.util.Log
 import com.unilim.iut.truckers.controller.WhiteListController
 import com.unilim.iut.truckers.model.PhoneNumber
 import com.unilim.iut.truckers.service.SmsReceiverService
@@ -43,5 +37,37 @@ class MainActivity : Activity() {
 
     override fun onDestroy() {
         super.onDestroy()
+    }
+
+    private fun createWhitelistFromJson(context: Context?) {
+        val filePath = "whitelist.json"
+
+        // Verification of the existence of the JSON file
+        val file = File(context?.filesDir, filePath)
+        if (file.exists()) {
+            Log.d("SMSReceiver", "Le fichier JSON existe déjà : $filePath")
+            return
+        }
+
+        // Whitelist creation
+        val whitelist = listOf(PhoneNumber("0123456789").phoneNumber, PhoneNumber("0987654321").phoneNumber, PhoneNumber("0555555555").phoneNumber)
+
+        // JSON object creation
+        val jsonObject = JSONObject()
+        jsonObject.put("whitelist", JSONArray(whitelist))
+
+        // Writing the JSON object in the JSON file
+        try {
+            // Opening the JSON file
+            val outputStream: FileOutputStream? = context?.openFileOutput(filePath, Context.MODE_PRIVATE)
+
+            // Writing the JSON object in the JSON file
+            outputStream?.write(jsonObject.toString(4).toByteArray())
+            outputStream?.close()
+
+            Log.d("SMSReceiver","Fichier JSON sauvegardé avec succès : $filePath")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
