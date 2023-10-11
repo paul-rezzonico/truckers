fdefca2cc78d3204b25fce2b585de74ca55fe1b0
Paul Rezzonico <paul.rezzonico@gmail.com>
2023-10-11 08:57:45 +0200

    refact(PhoneNumber) remove hash code and equals from data class

--- app/src/main/java/com/unilim/iut/truckers/model/PhoneNumber.kt
+++ app/src/main/java/com/unilim/iut/truckers/model/PhoneNumber.kt
@@ -3,28 +3,14 @@ package com.unilim.iut.truckers.model
 data class PhoneNumber(val phoneNumber: String) {
     init {
         require(isValidFrenchPhoneNumberFormat(phoneNumber)) {
-            "French phone number format is invalid"
+            "Le numéro de téléphone doit être au format international français"
         }
     }
 
     private fun isValidFrenchPhoneNumberFormat(phoneNumber: String): Boolean {
-        // French phone number format is 0X XX XX XX XX where X is a digit between 0 and 9 (included)
+        // Numéro de téléphone français au format international :
+        //  0X XX XX XX XX (10 chiffres) avec X compris entre 1 et 9
         val regex = """0[1-9]\d{8}"""
         return phoneNumber.matches(regex.toRegex())
-    }
-
-    override fun toString(): String {
-        return phoneNumber
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (other == null || other !is PhoneNumber) {
-            return false
-        }
-        return phoneNumber == other.phoneNumber
-    }
-
-    override fun hashCode(): Int {
-        return phoneNumber.hashCode()
     }
 }
