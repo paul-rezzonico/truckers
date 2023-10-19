b5a0cc799a938e47594d5a0b36bbc63bcb98bc3b
Paul Rezzonico <paul.rezzonico@gmail.com>
2023-10-19 16:00:58 +0200

    Refact(Message) refact message class, clean code

--- app/src/main/java/com/unilim/iut/truckers/model/Message.kt
+++ app/src/main/java/com/unilim/iut/truckers/model/Message.kt
@@ -1,10 +1,11 @@
 package com.unilim.iut.truckers.model
 
-data class Message(val phoneNumber: PhoneNumber,
-                   val message: String,
-                   val dateReception: String,
-                   val id: Int = nextId++,
-                   var estSynchro: Boolean = false
+data class Message(
+    val phoneNumber: PhoneNumber,
+    val message: String,
+    val dateReception: String,
+    val id: Int = nextId++,
+    var estSynchro: Boolean = false
 ) {
     companion object {
         private var nextId = 1
