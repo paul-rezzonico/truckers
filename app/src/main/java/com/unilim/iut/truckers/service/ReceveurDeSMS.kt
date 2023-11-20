7a36d446089a868ca9fdb4105d43c516abc99f13
Paul REZZONICO <paul.rezzonico@gmail.com>
2023-11-20 21:34:48 +0100

    Fix(Phone number) fix error where app was crashing when phone number wasn't in good format

--- app/src/main/java/com/unilim/iut/truckers/service/ReceveurDeSMS.kt
+++ app/src/main/java/com/unilim/iut/truckers/service/ReceveurDeSMS.kt
@@ -14,10 +14,10 @@ import com.unilim.iut.truckers.modele.Message
 
 class ReceveurDeSMS : BroadcastReceiver() {
 
-    private val controleurListeBlanche = ListeBlancheControleur();
-    private val controleurMessage = MessageControleur();
-    private val controleurMotCle = MotCleControleur();
-    private val controleurLogcat = LogcatControleur();
+    private val controleurListeBlanche = ListeBlancheControleur()
+    private val controleurMessage = MessageControleur()
+    private val controleurMotCle = MotCleControleur()
+    private val controleurLogcat = LogcatControleur()
 
     override fun onReceive(contexte: Context?, intention: Intent?) {
         if (intention != null) {
@@ -28,46 +28,50 @@ class ReceveurDeSMS : BroadcastReceiver() {
                 for (message in messages) {
                     Log.d("TruckerService", "--------------------------------------")
                     controleurLogcat.ecrireDansFichierLog("--------------------------------------")
-                    val numeroEmetteur = message.originatingAddress?.let { NumeroTelephone(it) }
-                    val corpsMessage = message.messageBody
+                    val numeroEmetteur: NumeroTelephone?
+                    try {
+                        numeroEmetteur = message.originatingAddress?.let { NumeroTelephone(it) }
+                        val corpsMessage = message.messageBody
 
-                    val listeBlanche = controleurListeBlanche.chargementListeBlanche(contexte, false)
-                    val numeroAdmin = controleurListeBlanche.chargementListeBlanche(contexte, true)
+                        val listeBlanche = controleurListeBlanche.chargementListeBlanche(contexte, false)
+                        val numeroAdmin = controleurListeBlanche.chargementListeBlanche(contexte, true)
 
-                    if (numeroEmetteur != null) {
-                        when(numeroEmetteur.numeroTelephone) {
-                            in listeBlanche.toString() -> {
-                                Log.d("TruckerService", "Message de la liste blanche")
-                                controleurLogcat.ecrireDansFichierLog("Message de la liste blanche")
+                        if (numeroEmetteur != null) {
+                            when(numeroEmetteur.numeroTelephone) {
+                                in listeBlanche.toString() -> {
+                                    Log.d("TruckerService", "Message de la liste blanche")
+                                    controleurLogcat.ecrireDansFichierLog("Message de la liste blanche")
 
-                                if (controleurMotCle.verificationMotsCles(contexte, corpsMessage)) {
-                                    controleurMessage.ajoutMessageDansJsonBonMessage(contexte, Message(numeroEmetteur, corpsMessage, message.timestampMillis.toString()))
-                                } else {
-                                    controleurMessage.ajoutMessageDansMauvaisJsonMessage(contexte, Message(numeroEmetteur, corpsMessage, message.timestampMillis.toString()))
+                                    if (controleurMotCle.verificationMotsCles(contexte, corpsMessage)) {
+                                        controleurMessage.ajoutMessageDansJsonBonMessage(contexte, Message(numeroEmetteur, corpsMessage, message.timestampMillis.toString()))
+                                    } else {
+                                        controleurMessage.ajoutMessageDansMauvaisJsonMessage(contexte, Message(numeroEmetteur, corpsMessage, message.timestampMillis.toString()))
+                                    }
                                 }
-                            }
 
-                            in numeroAdmin.toString() -> {
-                                Log.d("TruckerService", "Message de l'administrateur")
-                                controleurLogcat.ecrireDansFichierLog("Message de l'administrateur")
+                                in numeroAdmin.toString() -> {
+                                    Log.d("TruckerService", "Message de l'administrateur")
+                                    controleurLogcat.ecrireDansFichierLog("Message de l'administrateur")
 
-                                if (controleurMotCle.verificationMotsClesAdmin(contexte, corpsMessage)) {
-                                    controleurMessage.ajoutMessageDansJsonBonMessage(contexte, Message(numeroEmetteur, corpsMessage, message.timestampMillis.toString()))
-                                    controleurMessage.actionMessage(contexte, Message(numeroEmetteur, corpsMessage, message.timestampMillis.toString()))
-                                } else {
-                                    controleurMessage.ajoutMessageDansMauvaisJsonMessage(contexte, Message(numeroEmetteur, corpsMessage, message.timestampMillis.toString()))
+                                    if (controleurMotCle.verificationMotsClesAdmin(corpsMessage)) {
+                                        controleurMessage.ajoutMessageDansJsonBonMessage(contexte, Message(numeroEmetteur, corpsMessage, message.timestampMillis.toString()))
+                                        controleurMessage.actionMessage(contexte, Message(numeroEmetteur, corpsMessage, message.timestampMillis.toString()))
+                                    } else {
+                                        controleurMessage.ajoutMessageDansMauvaisJsonMessage(contexte, Message(numeroEmetteur, corpsMessage, message.timestampMillis.toString()))
+                                    }
                                 }
-                            }
 
-                            else -> {
-                                Log.d("TruckerService", "Message Invalide")
-                                controleurLogcat.ecrireDansFichierLog("Message Invalide")
+                                else -> {
+                                    Log.d("TruckerService", "Message Invalide")
+                                    controleurLogcat.ecrireDansFichierLog("Message Invalide")
+                                }
                             }
                         }
+                    } catch (e: Exception) {
+                        Log.d("TruckerService", e.message.toString())
+                        controleurLogcat.ecrireDansFichierLog(e.message.toString())
                     }
                 }
-
-
             }
         }
     }
