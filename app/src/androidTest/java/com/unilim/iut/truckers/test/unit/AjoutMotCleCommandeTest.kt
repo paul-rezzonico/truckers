6a4e4c94f6336553696e96193bb1df643da58478
Paul Rezzonico Ubuntu <paul.rezzo@ubuntu.com>
2023-11-23 19:31:35 +0100

    Fix(NumeroTelephone) fix phone number issue with +33

--- app/src/androidTest/java/com/unilim/iut/truckers/test/unit/AjoutMotCleCommandeTest.kt
+++ app/src/androidTest/java/com/unilim/iut/truckers/test/unit/AjoutMotCleCommandeTest.kt
@@ -1,8 +1,9 @@
 package com.unilim.iut.truckers.test.unit
 
-import com.unilim.iut.truckers.commande.AjoutMotCleCommande
 import android.content.Context
-import android.util.Log
+import com.unilim.iut.truckers.commande.AjoutMotCleCommande
+import org.json.JSONArray
+import org.json.JSONObject
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito
@@ -10,44 +11,60 @@ import java.io.File
 
 class AjoutMotCleCommandeTest {
 
-    private lateinit var context: Context
     private lateinit var tempFile: File
 
     @Before
     fun setUp() {
-        context = Mockito.mock(Context::class.java)
+        val mockContext = Mockito.mock(Context::class.java)
 
-        // Créer un fichier temporaire et écrire les données initiales
-        tempFile = File.createTempFile("testMotsCles", ".json")
-        tempFile.writeText("{\"mots_cles\": [\"motCleExistant\"]}")
+        val classLoader = javaClass.classLoader
+        val testFile = classLoader?.getResource("MotsCles.json")?.file?.let { File(it) }
 
-        // Configurer le mock de Context pour retourner le chemin du fichier temporaire
-        Mockito.`when`(context.filesDir).thenReturn(tempFile.parentFile)
+        if (testFile != null) {
+            Mockito.`when`<Any>(mockContext.filesDir).thenReturn(testFile.parentFile)
+        }
     }
 
     @Test
-    fun devraitRetournerFalseSiLeMotCleExisteDeja() {
-        val commande = AjoutMotCleCommande(context, "motCleExistant")
-        val resultat = commande.executer()
-        if (context.filesDir == tempFile.parentFile) {
-            assert(true)
-            println("Résultat du test : $resultat")
-            Log.i("TEST", "Résultat du test : $resultat")
-        } else {
-            assert(false)
+    fun devraitRetournerTrueSiLeMotCleAEteAjoute() {
+        val mockContext = Mockito.mock(Context::class.java)
+        val ajoutMotCleCommande = AjoutMotCleCommande(mockContext, "test")
+
+        val classLoader = javaClass.classLoader
+        val testFile = classLoader?.getResource("MotsCles.json")?.file?.let { File(it) }
+
+        if (testFile != null) {
+            Mockito.`when`<Any>(mockContext.filesDir).thenReturn(testFile.parentFile)
         }
 
-        // Nettoyer le fichier temporaire après le test
-        tempFile.delete()
-    }
+        val resultat = ajoutMotCleCommande.executer()
 
-    @Test
-    fun devraitRetournerTrueSiLeMotCleNExistePas() {
-        val commande = AjoutMotCleCommande(context, "motCleInexistant")
-        val resultat = commande.executer()
         assert(resultat)
-
-        // Nettoyer le fichier temporaire après le test
-        tempFile.delete()
     }
+
+    private fun doitEcrireLesMotsClee() {
+        val keywords: JSONArray = JSONArray();
+        keywords.put("RENDEZ-VOUS");
+        keywords.put("RDV");
+        keywords.put("LIVRAISON");
+        keywords.put("LIV");
+        keywords.put("URGENT");
+        keywords.put("URG");
+        keywords.put("INFORMATION");
+        keywords.put("INF")
+
+        // Create a JSON Object to hold the array
+        val jsonObject: JSONObject = JSONObject()
+        jsonObject.put("mots_cles", keywords);
+
+        val testFile = javaClass.classLoader?.getResource("MotsCles.json")?.file?.let { File(it) }
+
+        if (testFile != null) {
+            val fluxSortie = testFile.outputStream()
+            fluxSortie.write(jsonObject.toString(4).toByteArray())
+            fluxSortie.close()
+        }
+    }
+
+    private
 }
