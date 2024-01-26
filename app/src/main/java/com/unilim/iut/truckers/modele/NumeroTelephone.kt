3386cd4fd4763be9fd9ae7c2a3b7a3812718b51e
BoiteEnCarton <91901038+BoiteEnCarton@users.noreply.github.com>
2024-01-26 11:31:52 +0100

    Fix commentaire numéro de téléphone (#13)

--- app/src/main/java/com/unilim/iut/truckers/modele/NumeroTelephone.kt
+++ app/src/main/java/com/unilim/iut/truckers/modele/NumeroTelephone.kt
@@ -12,7 +12,7 @@ data class NumeroTelephone(var numeroTelephone: String) {
 
     private fun estUnNumeroFrancaisValide(numeroTelephone: String): Boolean {
         // Numéro de téléphone français au format international :
-        //  0X XX XX XX XX (10 chiffres) avec X compris entre 1 et 9
+        //  0X XX XX XX XX (10 chiffres) avec X compris entre 0 et 9, sauf le premier qui doit valoir entre 1 et 9
         val regex = """0[1-9]\d{8}"""
         val regex33 = """\+33[1-9]\d{8}"""
         return numeroTelephone.matches(regex.toRegex()) || numeroTelephone.matches(regex33.toRegex())
