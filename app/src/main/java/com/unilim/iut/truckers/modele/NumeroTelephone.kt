ed517c1567b221c63aa5f862b4a3c3d69346d725
Florian Rajoye <florian.rajoye@etu.unilim.fr>
2024-01-26 09:54:52 +0100

    fix de la double vérif

--- app/src/main/java/com/unilim/iut/truckers/modele/NumeroTelephone.kt
+++ app/src/main/java/com/unilim/iut/truckers/modele/NumeroTelephone.kt
@@ -2,11 +2,11 @@ package com.unilim.iut.truckers.modele
 
 data class NumeroTelephone(var numeroTelephone: String) {
     init {
-        require(estUnNumeroFrancaisValide(numeroTelephone)) {
-            "Le numéro de téléphone doit être au format international français : $numeroTelephone"
-        }
         if (numeroTelephone.substring(0, 3) == "+33") {
             numeroTelephone = "0" + numeroTelephone.substring(3)
+        }
+        require(estUnNumeroFrancaisValide(numeroTelephone)) {
+            "Le numéro de téléphone doit être au format international français : $numeroTelephone"
         }
     }
 
@@ -14,7 +14,6 @@ data class NumeroTelephone(var numeroTelephone: String) {
         // Numéro de téléphone français au format international :
         //  0X XX XX XX XX (10 chiffres) avec X compris entre 1 et 9
         val regex = """0[1-9]\d{8}"""
-        val regex33 = """\+33[1-9]\d{8}"""
-        return numeroTelephone.matches(regex.toRegex()) || numeroTelephone.matches(regex33.toRegex())
+        return numeroTelephone.matches(regex.toRegex())
     }
 }
